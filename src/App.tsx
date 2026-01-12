9fe6461732417462efe9a998e07e670fb5dc3bc7
aasish limbu <146429145+aasishdeveloper@users.noreply.github.com>
2026-01-12 20:39:45 +0545

    change

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,9 @@
-import {
-  NavigationMenu,
-  NavigationMenuContent,
-  NavigationMenuItem,
-  NavigationMenuLink,
-  NavigationMenuList,
-  NavigationMenuTrigger,
- 
-} from "../src/components/ui/navigation-menu"
+import { Button } from "../src/components/ui/button"
 
 function App() {
   return (
     <div className="flex min-h-svh flex-col items-center justify-center">
-      <NavigationMenu>
-  <NavigationMenuList>
-    <NavigationMenuItem>
-      <NavigationMenuTrigger>Item One</NavigationMenuTrigger>
-      <NavigationMenuContent>
-        <NavigationMenuLink>Link</NavigationMenuLink>
-      </NavigationMenuContent>
-    </NavigationMenuItem>
-  </NavigationMenuList>
-</NavigationMenu>
+      <Button>Click me</Button>
     </div>
   )
 }
